efd2d9f318a462dbcb2bf6149aca69c8d472226f
viraj7702 <102776442+viraj7702@users.noreply.github.com>
2022-06-08 02:05:11 -0400

    Add files via upload

---
+++ my_vm64.h
@@ -0,0 +1,57 @@
+#ifndef MY_VM_H_INCLUDED
+#define MY_VM_H_INCLUDED
+#include <stdbool.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <pthread.h> //comment
+#include <math.h>
+#include <string.h>
+
+//Assume the address space is 32 bits, so the max memory size is 4GB
+//Page size is 4KB
+
+//Add any important includes here which you may need
+
+#define PGSIZE 4096
+
+// Maximum size of virtual memory
+#define MAX_MEMSIZE 4ULL*1024*1024*1024
+
+// Size of "physcial memory"
+#define MEMSIZE 1024*1024*1024
+
+// Represents a page table entry
+typedef unsigned long long pte_t;
+
+// Represents a page directory entry
+typedef unsigned long long pde_t;
+
+#define TLB_ENTRIES 100//512
+
+//Structure to represents TLB
+struct TLB {
+    /*Assume your TLB is a direct mapped TLB with number of entries as TLB_ENTRIES
+    * Think about the size of each TLB entry that performs virtual to physical
+    * address translation.
+    */
+	unsigned long long int VPN;
+	unsigned long long int physAddr;
+
+};
+struct TLB tlb[TLB_ENTRIES];
+
+
+void set_physical_mem();
+void* translate(pde_t ****pgdir, void *va);
+int page_map(pde_t ****pgdir, void *va, void* pa);
+bool check_in_tlb(void *va);
+void put_in_tlb(void *va, void *pa);
+void *t_malloc(unsigned int num_bytes);
+void *next_avail_phys_page();
+void t_free(void *va, int size);
+void put_value(void *va, void *val, int size);
+void get_value(void *va, void *val, int size);
+void mat_mult(void *mat1, void *mat2, int size, void *answer);
+void print_TLB_missrate();
+
+#endif
